d4971a08653399f93b1a686507be7c1fa8f080e2
Cedric Debattista <cdebatista@novibet.com>
2021-02-17 13:07:00 +0100

    Added an extra Cotroller

---
+++ ATestWebApi/Controllers/AdditionalController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ATestWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdditionalController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        public AdditionalController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
