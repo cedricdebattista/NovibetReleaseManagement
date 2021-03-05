aea317fcf52a34765956068ad55da4d3a3d13a02
Cedric Debattista <cdebatista@novibet.com>
2021-03-05 08:14:19 +0100

    TES-28 New property

--- ATestWebApi/AditionalForecast.cs
+++ ATestWebApi/AditionalForecast.cs
@@ -4,6 +4,7 @@ namespace ATestWebApi
 {
     public class AditionalForecast:WeatherForecast
     {
+        public int IntegerTest { get; set; } = 11;
         public string New { get; set; } = "NEW";
         public string New2 { get; set; } = "NEW2";
         public string Hello { get; set; } = "Hello";
